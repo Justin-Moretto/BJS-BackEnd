9f890f795c31b3586b261d35db2279a56b8de002
justin-moretto <JustinMorettoMTL@gmail.com>
2021-02-03 18:20:05 +0000

    commented out dotenv

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-require('dotenv').config();
+//require('dotenv').config();
 
 const connectionString = `postgres://jwpbliytxqejai:8eb26fe27eaadbeb2cdb6115066640f89ad5cf58111d4e156910482e01ea301f@ec2-100-24-165-208.compute-1.amazonaws.com:5432/dpmhsqa1r6gbb` ;
 
