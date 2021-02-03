7cb2cd61ed52ce04b2cd171fc10cab714b426cc9
justin-moretto <JustinMorettoMTL@gmail.com>
2021-02-03 17:10:27 +0000

    setting up db for heroku

--- db/index.js
+++ db/index.js
@@ -1,18 +1,12 @@
 const pg = require('pg');
 require('dotenv').config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable` ;
+const connectionString = `postgres://jwpbliytxqejai:8eb26fe27eaadbeb2cdb6115066640f89ad5cf58111d4e156910482e01ea301f@ec2-100-24-165-208.compute-1.amazonaws.com:5432/dpmhsqa1r6gbb?sslmode=disable` ;
 
 const client = new pg.Client({
     connectionString: connectionString || process.env.DATABASE_URL,
 });
 
-console.log( `Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}` );
 client.connect();
-
-// client.query('SELECT * FROM users', (err, res) => {
-//     console.log(err ? err.stack : res.rows) // Testing db query from back-end!
-//     client.end()
-//   })
 
 module.exports = client;
