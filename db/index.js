f5f41c01ad8f92c368f2f62a05d35760eadb37a4
justin-moretto <JustinMorettoMTL@gmail.com>
2021-02-03 17:38:28 +0000

    adjusted connectionString

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const pg = require('pg');
 require('dotenv').config();
 
-const connectionString = `postgres://jwpbliytxqejai:8eb26fe27eaadbeb2cdb6115066640f89ad5cf58111d4e156910482e01ea301f@ec2-100-24-165-208.compute-1.amazonaws.com:5432/dpmhsqa1r6gbb?sslmode=disable` ;
+const connectionString = `postgres://jwpbliytxqejai:8eb26fe27eaadbeb2cdb6115066640f89ad5cf58111d4e156910482e01ea301f@ec2-100-24-165-208.compute-1.amazonaws.com:5432/dpmhsqa1r6gbb` ;
 
 const client = new pg.Client({
-    connectionString: connectionString || process.env.DATABASE_URL,
+    connectionString: connectionString, /* || process.env.DATABASE_URL*/
+    ssl: { rejectUnauthorized: false }
 });
 
 client.connect();
